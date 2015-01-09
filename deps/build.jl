88cfc4f73ad5b3c64bc35d2ebaf843c01c9fcb47
Tony Kelman <tony@kelman.net>
2015-01-09 11:27:11 -0800

    reuse Cbc, Ipopt, and deps from those packages

--- deps/build.jl
+++ deps/build.jl
@@ -22,12 +22,29 @@ prefix = joinpath(BinDeps.depsdir(libOS), "usr")
 patchdir = BinDeps.depsdir(libOS)
 srcdir = joinpath(BinDeps.depsdir(libOS), "src", "OS-$version")
 
+ENV2 = copy(ENV)
+ENV2["PKG_CONFIG_PATH"] = Pkg.dir("Cbc","deps","usr","lib","pkgconfig") *
+    ":" * Pkg.dir("Ipopt","deps","usr","lib","pkgconfig")
 provides(SimpleBuild,
     (@build_steps begin
         GetSources(libOS)
         @build_steps begin
             ChangeDirectory(srcdir)
-            `./configure --prefix=$prefix --enable-dependency-linking`
+            setenv(`./configure --prefix=$prefix --enable-dependency-linking
+                --with-coinutils-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lCoinUtils"
+                --with-coinutils-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
+                --with-osi-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lOsi"
+                --with-osi-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
+                --with-clp-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lClp"
+                --with-clp-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
+                --with-cgl-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lCgl"
+                --with-cgl-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
+                --with-cbc-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lCbc"
+                --with-cbc-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
+                --with-blas="-L$(Pkg.dir("Ipopt","deps","usr","lib")) -lcoinblas"
+                --with-lapack="-L$(Pkg.dir("Ipopt","deps","usr","lib")) -lcoinlapack"
+                --with-mumps-lib="-L$(Pkg.dir("Ipopt","deps","usr","lib")) -lcoinmumps"
+                --with-ipopt-lib="-L$(Pkg.dir("Ipopt","deps","usr","lib")) -lipopt"`, ENV2)
             `make install`
         end
     end), [libOS], os = :Unix)

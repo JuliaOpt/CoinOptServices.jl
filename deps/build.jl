ee02970d82325ea5f5baa531c699de7bb9342507
Tony Kelman <tony@kelman.net>
2015-01-09 11:53:41 -0800

    separate config flags between linux and osx

--- deps/build.jl
+++ deps/build.jl
@@ -23,28 +23,49 @@ patchdir = BinDeps.depsdir(libOS)
 srcdir = joinpath(BinDeps.depsdir(libOS), "src", "OS-$version")
 
 ENV2 = copy(ENV)
-ENV2["PKG_CONFIG_PATH"] = Pkg.dir("Cbc","deps","usr","lib","pkgconfig") *
-    ":" * Pkg.dir("Ipopt","deps","usr","lib","pkgconfig")
+@linux_only begin
+    ENV2["PKG_CONFIG_PATH"] = Pkg.dir("Cbc","deps","usr","lib","pkgconfig") *
+        ":" * Pkg.dir("Ipopt","deps","usr","lib","pkgconfig")
+    configflags = `--with-coinutils-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lCoinUtils"
+        --with-coinutils-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
+        --with-osi-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lOsi"
+        --with-osi-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
+        --with-clp-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lClp"
+        --with-clp-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
+        --with-cgl-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lCgl"
+        --with-cgl-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
+        --with-cbc-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lCbc"
+        --with-cbc-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
+        --with-blas="-L$(Pkg.dir("Ipopt","deps","usr","lib")) -lcoinblas"
+        --with-lapack="-L$(Pkg.dir("Ipopt","deps","usr","lib")) -lcoinlapack"
+        --with-mumps-lib="-L$(Pkg.dir("Ipopt","deps","usr","lib")) -lcoinmumps"
+        --with-ipopt-lib="-L$(Pkg.dir("Ipopt","deps","usr","lib")) -lipopt"`
+end
+@osx_only begin
+    ENV2["PKG_CONFIG_PATH"] = joinpath(Homebrew.prefix(),"lib","pkgconfig"))
+    configflags = `--with-coinutils-lib="-L$(joinpath(Homebrew.prefix(),"lib")) -lCoinUtils"
+        --with-coinutils-incdir=$(joinpath(Homebrew.prefix(),"include","coin"))
+        --with-osi-lib="-L$(joinpath(Homebrew.prefix(),"lib")) -lOsi"
+        --with-osi-incdir=$(joinpath(Homebrew.prefix(),"include","coin"))
+        --with-clp-lib="-L$(joinpath(Homebrew.prefix(),"lib")) -lClp"
+        --with-clp-incdir=$(joinpath(Homebrew.prefix(),"include","coin"))
+        --with-cgl-lib="-L$(joinpath(Homebrew.prefix(),"lib")) -lCgl"
+        --with-cgl-incdir=$(joinpath(Homebrew.prefix(),"include","coin"))
+        --with-cbc-lib="-L$(joinpath(Homebrew.prefix(),"lib")) -lCbc"
+        --with-cbc-incdir=$(joinpath(Homebrew.prefix(),"include","coin"))
+        --with-blas="-L$(joinpath(Homebrew.prefix(),"lib")) -lcoinblas"
+        --with-lapack="-L$(joinpath(Homebrew.prefix(),"lib")) -lcoinlapack"
+        --with-mumps-lib="-L$(joinpath(Homebrew.prefix(),"lib")) -lcoinmumps"
+        --with-ipopt-lib="-L$(joinpath(Homebrew.prefix(),"lib")) -lipopt"`
+end
+
 provides(SimpleBuild,
     (@build_steps begin
         GetSources(libOS)
         @build_steps begin
             ChangeDirectory(srcdir)
             setenv(`./configure --prefix=$prefix --enable-dependency-linking
-                --with-coinutils-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lCoinUtils"
-                --with-coinutils-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
-                --with-osi-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lOsi"
-                --with-osi-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
-                --with-clp-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lClp"
-                --with-clp-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
-                --with-cgl-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lCgl"
-                --with-cgl-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
-                --with-cbc-lib="-L$(Pkg.dir("Cbc","deps","usr","lib")) -lCbc"
-                --with-cbc-incdir=$(Pkg.dir("Cbc","deps","usr","include","coin"))
-                --with-blas="-L$(Pkg.dir("Ipopt","deps","usr","lib")) -lcoinblas"
-                --with-lapack="-L$(Pkg.dir("Ipopt","deps","usr","lib")) -lcoinlapack"
-                --with-mumps-lib="-L$(Pkg.dir("Ipopt","deps","usr","lib")) -lcoinmumps"
-                --with-ipopt-lib="-L$(Pkg.dir("Ipopt","deps","usr","lib")) -lipopt"`, ENV2)
+                $configflags`, ENV2)
             `make install`
         end
     end), [libOS], os = :Unix)

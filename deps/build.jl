9022481baddc667bfc1a2f0807832f97e55c7832
Tony Kelman <tony@kelman.net>
2015-01-10 02:04:35 -0800

    try using ipopt and cbc packages again
    using patched ipopt.pc

--- deps/build.jl
+++ deps/build.jl
@@ -9,46 +9,60 @@ provides(Sources, URI("http://www.coin-or.org/download/source/OS/OS-$version.tgz
     [libOS], os = :Unix)
 
 @windows_only begin
-    #using WinRPM
+    using WinRPM
     #provides(WinRPM.RPM, "OptimizationServices", [libOS], os = :Windows)
     # TODO until WinRPM is all ready, download win32 binary of CoinAll?
     # May be impossible to satisfy BinDeps library_dependency for win64 Julia in that case
+    cbcdir = joinpath(WinRPM.installdir, "usr", Sys.MACHINE, "sys-root", "mingw", "bin")
+    ipoptdir = cbcdir
 end
 
 @osx_only begin
-    #using Homebrew
+    using Homebrew
     #provides(Homebrew.HB, "OptimizationServices", [libOS], os = :Darwin)
+    cbcdir = Homebrew.prefix()
+    ipoptdir = cbcdir
+    # fixup ipopt.pc
+    download("https://gist.githubusercontent.com/staticfloat/a0b360cf59f12c973064/raw/93aa7f050b02dad31a391d4ec259d186c6b2c607/ipopt.pc",
+        joinpath(ipoptdir, "lib", "pkgconfig", "ipopt.pc"))
+end
+
+@linux_only begin
+    cbcdir = Pkg.dir("Cbc", "deps", "usr")
+    ipoptdir = Pkg.dir("Ipopt", "deps", "usr")
 end
 
 prefix = joinpath(BinDeps.depsdir(libOS), "usr")
 patchdir = BinDeps.depsdir(libOS)
 srcdir = joinpath(BinDeps.depsdir(libOS), "src", "OS-$version")
 
+ENV2 = copy(ENV)
+@unix_only ENV2["PKG_CONFIG_PATH"] = joinpath(cbcdir, "lib", "pkgconfig") *
+    ":" * joinpath(ipoptdir, "lib", "pkgconfig")
+
 provides(SimpleBuild,
     (@build_steps begin
         GetSources(libOS)
         @build_steps begin
             ChangeDirectory(srcdir)
-            @build_steps begin
-                ChangeDirectory(joinpath(srcdir, "ThirdParty", "ASL"))
-                `./get.ASL`
-            end
-            @build_steps begin
-                ChangeDirectory(joinpath(srcdir, "ThirdParty", "Blas"))
-                `./get.Blas`
-            end
-            @build_steps begin
-                ChangeDirectory(joinpath(srcdir, "ThirdParty", "Lapack"))
-                `./get.Lapack`
-            end
-            @build_steps begin
-                ChangeDirectory(joinpath(srcdir, "ThirdParty", "Mumps"))
-                `./get.Mumps`
-            end
             `cat $patchdir/OS-clang.patch` |> `patch -p1`
-            `./configure --prefix=$prefix --enable-dependency-linking
-                coin_skip_warn_cflags=yes coin_skip_warn_cxxflags=yes coin_skip_warn_fflags=yes`
-            `make` |> "make.log"
+            setenv(`./configure --prefix=$prefix --enable-dependency-linking
+                coin_skip_warn_cflags=yes coin_skip_warn_cxxflags=yes coin_skip_warn_fflags=yes
+                --with-coinutils-lib="-L$(joinpath(cbcdir,"lib")) -lCoinUtils"
+                --with-coinutils-incdir=$(joinpath(cbcdir,"include","coin"))
+                --with-osi-lib="-L$(joinpath(cbcdir,"lib")) -lOsi -lCoinUtils"
+                --with-osi-incdir=$(joinpath(cbcdir,"include","coin"))
+                --with-clp-lib="-L$(joinpath(cbcdir,"lib")) -lClp -lOsiClp"
+                --with-clp-incdir=$(joinpath(cbcdir,"include","coin"))
+                --with-cgl-lib="-L$(joinpath(cbcdir,"lib")) -lCgl"
+                --with-cgl-incdir=$(joinpath(cbcdir,"include","coin"))
+                --with-cbc-lib="-L$(joinpath(cbcdir,"lib")) -lCbc"
+                --with-cbc-incdir=$(joinpath(cbcdir,"include","coin"))
+                --with-blas="-L$(joinpath(ipoptdir,"lib")) -lcoinblas"
+                --with-lapack="-L$(joinpath(ipoptdir,"lib")) -lcoinlapack"
+                --with-mumps-lib="-L$(joinpath(ipoptdir,"lib")) -lcoinmumps"
+                --with-ipopt-lib="-L$(joinpath(ipoptdir,"lib")) -lipopt"`, ENV2)
+            `make`
             `make -j1 install`
             `make test`
         end

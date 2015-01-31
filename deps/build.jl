e17cb7c61a09a7907b2c810774aeff805462ed57
Tony Kelman <tony@kelman.net>
2015-01-31 02:13:48 -0800

    shorten bindeps code

--- deps/build.jl
+++ deps/build.jl
@@ -13,20 +13,21 @@ provides(Sources, URI("http://www.coin-or.org/download/source/OS/OS-$version.tgz
     push!(WinRPM.sources, "http://download.opensuse.org/repositories/home:/kelman:/mingw-coinor/openSUSE_13.1")
     WinRPM.update()
     provides(WinRPM.RPM, "OptimizationServices", [libOS], os = :Windows)
-    cbcdir = joinpath(WinRPM.installdir, "usr", Sys.MACHINE, "sys-root", "mingw", "bin")
-    ipoptdir = cbcdir
+    cbclibdir = joinpath(WinRPM.installdir, "usr", Sys.MACHINE, "sys-root", "mingw", "bin")
+    ipoptlibdir = cbcdir
 end
 
 @osx_only begin
     using Homebrew
     provides(Homebrew.HB, "Optimizationservices", [libOS], os = :Darwin)
-    cbcdir = Homebrew.prefix()
-    ipoptdir = cbcdir
+    cbclibdir = joinpath(Homebrew.prefix(), "lib")
+    ipoptlibdir = cbcdir
 end
 
 @linux_only begin
-    cbcdir = Pkg.dir("Cbc", "deps", "usr")
-    ipoptdir = Pkg.dir("Ipopt", "deps", "usr")
+    # should probably be using deps/deps.jl for these
+    cbclibdir = Pkg.dir("Cbc", "deps", "usr", "lib")
+    ipoptlibdir = Pkg.dir("Ipopt", "deps", "usr", "lib")
 end
 
 prefix = joinpath(BinDeps.depsdir(libOS), "usr")
@@ -34,8 +35,9 @@ patchdir = BinDeps.depsdir(libOS)
 srcdir = joinpath(BinDeps.depsdir(libOS), "src", "OS-$version")
 
 ENV2 = copy(ENV)
-@unix_only ENV2["PKG_CONFIG_PATH"] = joinpath(cbcdir, "lib", "pkgconfig") *
-    ":" * joinpath(ipoptdir, "lib", "pkgconfig")
+@unix_only ENV2["PKG_CONFIG_PATH"] = joinpath(cbclibdir, "pkgconfig") *
+    ":" * joinpath(ipoptlibdir, "pkgconfig")
+cbcincdir = joinpath(cbclibdir, "..", "include", "coin")
 
 provides(SimpleBuild,
     (@build_steps begin
@@ -44,20 +46,20 @@ provides(SimpleBuild,
             ChangeDirectory(srcdir)
             setenv(`./configure --prefix=$prefix --enable-dependency-linking
                 coin_skip_warn_cflags=yes coin_skip_warn_cxxflags=yes coin_skip_warn_fflags=yes
-                --with-coinutils-lib="-L$(joinpath(cbcdir,"lib")) -lCoinUtils"
-                --with-coinutils-incdir=$(joinpath(cbcdir,"include","coin"))
-                --with-osi-lib="-L$(joinpath(cbcdir,"lib")) -lOsi -lCoinUtils"
-                --with-osi-incdir=$(joinpath(cbcdir,"include","coin"))
-                --with-clp-lib="-L$(joinpath(cbcdir,"lib")) -lClp -lOsiClp"
-                --with-clp-incdir=$(joinpath(cbcdir,"include","coin"))
-                --with-cgl-lib="-L$(joinpath(cbcdir,"lib")) -lCgl"
-                --with-cgl-incdir=$(joinpath(cbcdir,"include","coin"))
-                --with-cbc-lib="-L$(joinpath(cbcdir,"lib")) -lCbc"
-                --with-cbc-incdir=$(joinpath(cbcdir,"include","coin"))
-                --with-blas="-L$(joinpath(ipoptdir,"lib")) -lcoinblas"
-                --with-lapack="-L$(joinpath(ipoptdir,"lib")) -lcoinlapack"
-                --with-mumps-lib="-L$(joinpath(ipoptdir,"lib")) -lcoinmumps"
-                --with-ipopt-lib="-L$(joinpath(ipoptdir,"lib")) -lipopt"`, ENV2)
+                --with-coinutils-lib="-L$cbclibdir -lCoinUtils"
+                --with-coinutils-incdir=$cbcincdir
+                --with-osi-lib="-L$cbclibdir -lOsi -lCoinUtils"
+                --with-osi-incdir=$cbcincdir
+                --with-clp-lib="-L$cbclibdir -lClp -lOsiClp"
+                --with-clp-incdir=$cbcincdir
+                --with-cgl-lib="-L$cbclibdir -lCgl"
+                --with-cgl-incdir=$cbcincdir
+                --with-cbc-lib="-L$cbclibdir -lCbc"
+                --with-cbc-incdir=$cbcincdir
+                --with-blas="-L$ipoptlibdir -lcoinblas"
+                --with-lapack="-L$ipoptlibdir -lcoinlapack"
+                --with-mumps-lib="-L$ipoptlibdir -lcoinmumps"
+                --with-ipopt-lib="-L$ipoptlibdir -lipopt"`, ENV2)
             `make`
             `make -j1 install`
             `make test`

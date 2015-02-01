b9d81b6a92dc259c940a8306a7c82145cdb62d32
Tony Kelman <tony@kelman.net>
2015-02-01 02:45:35 -0800

    Incorporate parser patch for binary variable bounds
    from https://projects.coin-or.org/OS/changeset/4937
    
    (enum capitalization is different on 2.8.5)

--- deps/build.jl
+++ deps/build.jl
@@ -46,6 +46,7 @@ provides(SimpleBuild,
         @build_steps begin
             ChangeDirectory(builddir)
             `cat $patchdir/os-printlevel.patch` |> `patch -p1 -d ..`
+            `cat $patchdir/os-binarybounds.patch` |> `patch -p1 -d ..`
             setenv(`../configure --prefix=$prefix --enable-dependency-linking
                 coin_skip_warn_cflags=yes coin_skip_warn_cxxflags=yes coin_skip_warn_fflags=yes
                 --with-coinutils-lib="-L$cbclibdir -lCoinUtils"

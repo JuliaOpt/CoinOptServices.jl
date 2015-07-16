77cdc2dae426a08a39d51ebf19b3f8694234400c
Tony Kelman <tony@kelman.net>
2015-07-16 08:07:23 -0400

    force max_iter to 27

--- test/rocket.jl
+++ test/rocket.jl
@@ -7,7 +7,7 @@ for mod in (Model(solver=IpoptSolver(print_timing_statistics="yes")),
     Model(solver=IpoptNLSolver()),
     Model(solver=OsilSolver(OSOption("print_timing_statistics", "yes"),
         OSOption("print_level", 5),
-        OSOption("derivative_test", "second-order"))))
+        OSOption("max_iter", 27))))
 
 # Constants
 # Note that all parameters in the model have been normalized
